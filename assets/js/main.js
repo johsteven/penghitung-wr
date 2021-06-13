7bfd7762aa111e8e74de3bc11a2d4a6e611cefeb
johsteven <johsteven222004@gmail.com>
2021-06-14 02:46:39 +0700

    fix bug and add some features

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,38 @@ const resultText = document.querySelector("#resultText");
 
 // Functions
 function res() {
-    const resultNum = rumus(tMatch.value, tWr.value, wrReq.value);
-    const text = `Kamu memerlukan sekitar <b>${resultNum}</b> win tanpa lose untuk mendapatkan win rate <b>${wrReq.value}%</b>`;
-    resultText.innerHTML = text;
+    validation();
+}
+
+function validation() {
+    const resultNum = rumus(parseInt(tMatch.value), parseInt(tWr.value), parseInt(wrReq.value));
+    let text = "";
+    if (tMatch.value == "" || tWr.value == "" || wrReq.value == "") {
+        text = `Field harus diisi bro.`;
+        display(text);
+    } else if (parseInt(tMatch.value) < 0 || parseInt(tWr.value) < 0 || parseInt(wrReq.value) < 0) {
+        text = `Field tidak boleh lebih kecil dari 0`;
+        display(text);
+    } else if (tWr.value == "100" && wrReq.value == "100") {
+        text = `Kamu memerlukan sekitar <b>0</b> win tanpa lose untuk mendapatkan win rate <b>${wrReq.value}%</b>`;
+        display(text);
+    } else if (wrReq.value > 100 || tWr.value > 100) {
+        text = `WR tidak boleh lebih dari 100%`;
+        display(text);
+    } else if (resultNum >= 10000) {
+        text = `<b>WTF DAMAGE!!!</b> <br> Kamu memerlukan lebih dari <b>10.000</b> win tanpa lose untuk mendapatkan win rate <b>${wrReq.value}%</b>`;
+        display(text);
+    } else if (parseInt(tWr.value) > parseInt(wrReq.value)) {
+        text = `Kamu memerlukan sekitar <b>${-resultNum}</b> lose tanpa win untuk mendapatkan win rate <b>${wrReq.value}%</b>`;
+        display(text);
+    } else {
+        text = `Kamu memerlukan sekitar <b>${resultNum}</b> win tanpa lose untuk mendapatkan win rate <b>${wrReq.value}%</b>`;
+        display(text);
+    }
+}
+
+function display(text) {
+    return resultText.innerHTML = text;
 }
 
 function rumus(tMatch, tWr, wrReq) {
