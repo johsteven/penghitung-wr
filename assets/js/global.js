9d27d9878d5b1a769eed94346813e30eea3f7481
johsteven <johsteven222004@gmail.com>
2021-06-17 04:42:19 +0700

    saokdoas

--- assets/js/global.js
+++ assets/js/global.js
@@ -4,12 +4,11 @@ function welcomeMsg() {
 
     const rendi = `<a href="https://api.whatsapp.com/send?phone=6285867904766" target="_blank">kesini</a>`;
 
-    const msg1 = `<p class="animation mb-0 text-truncate">Buat yang mau pm fb, tiktok, ig disini pc ya</p>`;
-    const msg2 = `<p class="animation mb-0 text-truncate">Yang mau top up diamond ML bisa ${rendi} ya</p>`;
+    const msg1 = `<p class="animation mb-0 text-truncate">Yang mau top up diamond ML bisa ${rendi} ya</p>`;
+    const msg2 = `<p class="animation mb-0 text-truncate">Buat yang mau promosi fb, tiktok, ig pc WA ya</p>`;
     const msg3 = `<p class="animation mb-0 text-truncate">Jangan lupa follow sosmed gua dibawah ya</p>`;
-    const msg4 = `<p class="animation mb-0 text-truncate">Buat yang namanya mau dipajang disini, pc ya</p>`;
-    const msg5 = `<p class="animation mb-0 text-truncate">Bagi yang mau donasi ke saya, pc WA ya</p>`;
-    const msg6 = `<p class="animation mb-0 text-truncate">Follow ML gw -> 'Stee.' rank mitik</p>`;
+    const msg4 = `<p class="animation mb-0 text-truncate">Bagi yang mau donasi ke saya, pc WA ya</p>`;
+    const msg5 = `<p class="animation mb-0 text-truncate">Follow ML gw -> 'Stee.' rank mitik</p>`;
     // const msg6 = `<p class="animation mb-0 text-truncate">Ingat! WR hanyalah angka</p>`;
     // const msg7 = `<p class="animation mb-0 text-truncate">Aman sedikit dunia</p>`;
     // const msg8 = `<p class="animation mb-0 text-truncate">Jangan lupa istirahat coeg</p>`;
@@ -19,8 +18,8 @@ function welcomeMsg() {
     // const msg12 = `<p class="animation mb-0 text-truncate">Semangat party nya bro</p>`;
     // const msg13 = `<p class="animation mb-0 text-truncate">Semangat push ke mitik bro</p>`;
 
-    let arrayEl = [msg1, msg2, msg3, msg4, msg5, msg6];
-    let arrayMax = 6;
+    let arrayEl = [msg1, msg2, msg3, msg4, msg5];
+    let arrayMax = 5;
     setInterval(() => {
         welcomeMsgEl.innerHTML = arrayEl[array];
 
