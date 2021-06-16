7da1b21373f109459e4ed1302dfce0ce940a1bd1
johsteven <johsteven222004@gmail.com>
2021-06-17 03:02:08 +0700

    adokwaokdoa

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,22 +2,23 @@ function welcomeMsg() {
     const welcomeMsgEl = document.querySelector("#welcomeMsg");
     let array = 0;
 
-    const msg1 = `<p class="animation mb-0 text-truncate">Salam kenal buat user Malaysia</p>`;
-    const msg2 = `<p class="animation mb-0 text-truncate">Jangan lupa follow sosmed gua dibawah ya</p>`;
-    const msg3 = `<p class="animation mb-0 text-truncate">Buat yang mau namanya dipajang disini, pc ya</p>`;
-    const msg4 = `<p class="animation mb-0 text-truncate">Bagi yang mau donasi ke saya, pc WA ya</p>`;
-    const msg5 = `<p class="animation mb-0 text-truncate">Follow ML gw -> 'Stee.' rank mitik</p>`;
-    const msg6 = `<p class="animation mb-0 text-truncate">Ingat! WR hanyalah angka</p>`;
-    const msg7 = `<p class="animation mb-0 text-truncate">Aman sedikit dunia</p>`;
-    const msg8 = `<p class="animation mb-0 text-truncate">Jangan lupa istirahat coeg</p>`;
-    const msg9 = `<p class="animation mb-0 text-truncate">Semangat GB nya bro</p>`;
-    const msg10 = `<p class="animation mb-0 text-truncate">Lo user apa?</p>`;
-    const msg11 = `<p class="animation mb-0 text-truncate">Mabar yok</p>`;
-    const msg12 = `<p class="animation mb-0 text-truncate">Semangat party nya bro</p>`;
-    const msg13 = `<p class="animation mb-0 text-truncate">Semangat push ke mitik bro</p>`;
+    const msg1 = `<p class="animation mb-0 text-truncate">Buat yang mau pm fb, tiktok, ig disini pc ya</p>`;
+    const msg2 = `<p class="animation mb-0 text-truncate">Salam kenal buat user Malaysia</p>`;
+    const msg3 = `<p class="animation mb-0 text-truncate">Jangan lupa follow sosmed gua dibawah ya</p>`;
+    const msg4 = `<p class="animation mb-0 text-truncate">Buat yang namanya mau dipajang disini, pc ya</p>`;
+    const msg5 = `<p class="animation mb-0 text-truncate">Bagi yang mau donasi ke saya, pc WA ya</p>`;
+    const msg6 = `<p class="animation mb-0 text-truncate">Follow ML gw -> 'Stee.' rank mitik</p>`;
+    // const msg6 = `<p class="animation mb-0 text-truncate">Ingat! WR hanyalah angka</p>`;
+    // const msg7 = `<p class="animation mb-0 text-truncate">Aman sedikit dunia</p>`;
+    // const msg8 = `<p class="animation mb-0 text-truncate">Jangan lupa istirahat coeg</p>`;
+    // const msg9 = `<p class="animation mb-0 text-truncate">Semangat GB nya bro</p>`;
+    // const msg10 = `<p class="animation mb-0 text-truncate">Lo user apa?</p>`;
+    // const msg11 = `<p class="animation mb-0 text-truncate">Mabar yok</p>`;
+    // const msg12 = `<p class="animation mb-0 text-truncate">Semangat party nya bro</p>`;
+    // const msg13 = `<p class="animation mb-0 text-truncate">Semangat push ke mitik bro</p>`;
 
-    let arrayEl = [msg1, msg2, msg3, msg4, msg5, msg6, msg7, msg8, msg9, msg10, msg11, msg12, msg13];
-    let arrayMax = 13;
+    let arrayEl = [msg1, msg2, msg3, msg4, msg5, msg6];
+    let arrayMax = 6;
     setInterval(() => {
         welcomeMsgEl.innerHTML = arrayEl[array];
 
@@ -25,5 +26,5 @@ function welcomeMsg() {
         if (array >= arrayMax) {
             array = 0;
         }
-    }, 4000);
+    }, 4250);
 }
