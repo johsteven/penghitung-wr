4821426f76b4c91f37f3cf01f802fab02c873c64
johsteven <johsteven222004@gmail.com>
2021-06-20 04:22:05 +0700

    asdas

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,11 +6,12 @@ function welcomeMsg() {
     const linkTiktokGua = `<a href="https://vt.tiktok.com/ZSJQ5XuUu/" target="_blank">tiktok</a>`
 
     const msg1 = `<p class="animation mb-0 text-truncate">JOKI, TOP UP & GIFT SKIN ML di ${takapedia} ya</p>`;
-    const msg2 = `<p class="animation mb-0 text-truncate">Follow ${linkTiktokGua} gw ya</p>`;
-    const msg3 = `<p class="animation mb-0 text-truncate">Jangan lupa follow sosmed gua dibawah ya</p>`;
+    const msg2 = `<p class="animation mb-0 text-truncate">Takapedia dijamin amanah dan terpercaya</p>`;
+    const msg3 = `<p class="animation mb-0 text-truncate">Follow ${linkTiktokGua} gw ya</p>`;
+    const msg4 = `<p class="animation mb-0 text-truncate">Jangan lupa follow sosmed gua dibawah ya</p>`;
 
-    let arrayEl = [msg1, msg2, msg3];
-    let arrayMax = 3;
+    let arrayEl = [msg1, msg2, msg3, msg4];
+    let arrayMax = 4;
     setInterval(() => {
         welcomeMsgEl.innerHTML = arrayEl[array];
 
