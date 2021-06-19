b108729c2c919b3ac4dd4c56d231410a78e26380
johsteven <johsteven222004@gmail.com>
2021-06-20 04:30:31 +0700

    Aasd

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,15 +3,18 @@ function welcomeMsg() {
     let array = 0;
 
     const takapedia = `<a href="https://msha.ke/takapedia/" target="_blank">takapedia</a>`;
+    const takapediaIG = `<a href="https://www.instagram.com/taka_pedia/" target="_blank">taka_pedia</a>`;
+    const takapediaWA = `<a href="https://api.whatsapp.com/send?phone=6283111044458" target="_blank">takapedia</a>`;
     const linkTiktokGua = `<a href="https://vt.tiktok.com/ZSJQ5XuUu/" target="_blank">tiktok</a>`
 
     const msg1 = `<p class="animation mb-0 text-truncate">JOKI, TOP UP & GIFT SKIN ML di ${takapedia} ya</p>`;
-    const msg2 = `<p class="animation mb-0 text-truncate">Takapedia dijamin amanah dan terpercaya</p>`;
-    const msg3 = `<p class="animation mb-0 text-truncate">Follow ${linkTiktokGua} gw ya</p>`;
-    const msg4 = `<p class="animation mb-0 text-truncate">Jangan lupa follow sosmed gua dibawah ya</p>`;
+    const msg2 = `<p class="animation mb-0 text-truncate">IG: ${takapediaIG} / WA: ${takapediaWA}</p>`;
+    const msg3 = `<p class="animation mb-0 text-truncate">Takapedia dijamin amanah dan terpercaya</p>`;
+    const msg4 = `<p class="animation mb-0 text-truncate">Follow ${linkTiktokGua} gw ya</p>`;
+    const msg5 = `<p class="animation mb-0 text-truncate">Jangan lupa follow sosmed gua dibawah ya</p>`;
 
-    let arrayEl = [msg1, msg2, msg3, msg4];
-    let arrayMax = 4;
+    let arrayEl = [msg1, msg2, msg3, msg4, msg5];
+    let arrayMax = 5;
     setInterval(() => {
         welcomeMsgEl.innerHTML = arrayEl[array];
 
@@ -19,5 +22,5 @@ function welcomeMsg() {
         if (array >= arrayMax) {
             array = 0;
         }
-    }, 4250);
+    }, 4000);
 }
