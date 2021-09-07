354d22fc30976776ebb5246b56efecd3d1a12767
johsteven <johsteven222004@gmail.com>
2021-09-08 04:40:57 +0700

    asda

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,12 +5,12 @@ function welcomeMsg() {
     const takapedia = `<a href="https://msha.ke/takapedia/" target="_blank">takapedia</a>`;
     const takapediaIG = `<a href="https://www.instagram.com/taka_pedia/" target="_blank">taka_pedia</a>`;
     const takapediaWA = `<a href="https://api.whatsapp.com/send?phone=6287843920288" target="_blank">takapedia</a>`;
-    const linkTiktokGua = `<a href="https://vt.tiktok.com/ZSJQ5XuUu/" target="_blank">tiktok</a>`
+    const linkWeb = `Cobain web baru gw -> <a href="https://gudkuesyen.com" target="_blank">gudkuesyen.com</a>`
 
     const msg1 = `<p class="animation mb-0 text-truncate">JOKI, TOP UP & GIFT SKIN ML di ${takapedia} ya</p>`;
     const msg2 = `<p class="animation mb-0 text-truncate">IG: ${takapediaIG} / WA: ${takapediaWA}</p>`;
     const msg3 = `<p class="animation mb-0 text-truncate">Takapedia dijamin amanah dan terpercaya</p>`;
-    const msg4 = `<p class="animation mb-0 text-truncate">Follow ${linkTiktokGua} gw ya</p>`;
+    const msg4 = `<p class="animation mb-0 text-truncate">${linkWeb}`;
     const msg5 = `<p class="animation mb-0 text-truncate">Jangan lupa follow sosmed gua dibawah ya</p>`;
 
     let arrayEl = [msg1, msg2, msg3, msg4, msg5];
